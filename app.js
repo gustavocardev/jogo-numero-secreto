9ccbe723570ec096610890b8413e4ea7952af865
Gustavo Cardoso Machado <gustavo.cardoso@wickbold.com.br>
2025-10-20 12:55:31 -0300

    explorer

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
 // enquando chute não for igual ao n. s.
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
-
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        
+        tentativas++;
     }
+}
+
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+lert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativas}`);
+
+
+if(tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
 }
